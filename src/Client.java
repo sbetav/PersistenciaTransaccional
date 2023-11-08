4f213d3ea2cc7562ea38de35d6f6de658530fc9d
Santiago Betancur <santibeta82@gmail.com>
2023-11-07 21:48:23 -0500

    Comentarios javadoc

--- src/Client.java
+++ src/Client.java
@@ -4,75 +4,88 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+/**
+ * Esta clase representa un cliente que se conecta a un servidor a través de un socket.
+ */
 public class Client implements Runnable {
 
-	private Socket client;
-	private BufferedReader in;
-	private PrintWriter out;
-	private boolean done;
+    private Socket client;
+    private BufferedReader in;
+    private PrintWriter out;
+    private boolean done;
 
-	@Override
-	public void run() {
-		try {
-			client = new Socket("127.0.0.1", 8080);
-			out = new PrintWriter(client.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+    /**
+     * Método principal que crea una instancia del cliente y ejecuta el método run.
+     * @param args Argumentos de la línea de comandos (no se utilizan en este caso).
+     */
+    public static void main(String[] args) {
+    	Client client = new Client();
+    	client.run();
+    }
+    
+    /**
+     * El método run se ejecuta cuando se inicia un hilo para este cliente.
+     */
+    @Override
+    public void run() {
+        try {
+            client = new Socket("127.0.0.1", 8080);
+            out = new PrintWriter(client.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-			InputHandler inHandler = new InputHandler();
-			Thread t = new Thread(inHandler);
-			t.start();
+            InputHandler inHandler = new InputHandler();
+            Thread t = new Thread(inHandler);
+            t.start();
 
-			String inMessage;
-			while ((inMessage = in.readLine()) != null) {
-				System.out.println(inMessage);
-			}
+            String inMessage;
+            while ((inMessage = in.readLine()) != null) {
+                System.out.println(inMessage);
+            }
 
-		} catch (IOException e) {
-			shutdown();
-		}
+        } catch (IOException e) {
+            shutdown();
+        }
+    }
 
-	}
+    /**
+     * Este método se utiliza para cerrar las conexiones y limpiar recursos.
+     */
+    public void shutdown() {
+        done = true;
+        try {
+            in.close();
+            out.close();
+            if (!client.isClosed()) {
+                client.close();
+            }
+        } catch (IOException e) {
+            // ignore
+        }
+    }
 
-	public void shutdown() {
-		done = true;
-		try {
-			in.close();
-			out.close();
-			if (!client.isClosed()) {
-				client.close();
-			}
-		} catch (IOException e) {
-			// ignore
-		}
-	}
+    /**
+     * Esta clase interna se encarga de manejar la entrada del usuario desde la consola.
+     */
+    class InputHandler implements Runnable {
 
-	class InputHandler implements Runnable {
+        @Override
+        public void run() {
+            try {
+                BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+                while (!done) {
+                    String message = inReader.readLine();
+                    if (message.equalsIgnoreCase("chao")) {
+                        out.println("chao");
+                        inReader.close();
+                        shutdown();
+                    } else {
+                        out.println(message);
+                    }
+                }
+            } catch (IOException e) {
+                shutdown();
+            }
+        }
+    }
 
-		@Override
-		public void run() {
-			try {
-				BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
-				while (!done) {
-					String message = inReader.readLine();
-					if (message.equalsIgnoreCase("chao")) {
-						out.println("chao");
-						inReader.close();
-						shutdown();
-					} else {
-						out.println(message);
-					}
-				}
-			} catch (IOException e) {
-				shutdown();
-			}
-
-		}
-
-	}
-
-	
-	public static void main(String[] args) {
-		Client client = new Client();
-		client.run();
-	}
 }
--- src/Server.java
+++ src/Server.java
@@ -8,115 +8,154 @@ import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * Esta clase representa un servidor de chat que acepta conexiones de múltiples clientes.
+ */
 public class Server implements Runnable {
 
-	private ArrayList<ConnectionHandler> connections;
-	private ServerSocket server;
-	private boolean done;
-	private ExecutorService pool;
+    private ArrayList<ConnectionHandler> connections;
+    private ServerSocket server;
+    private boolean done;
+    private ExecutorService pool;
 
-	public Server() {
-		connections = new ArrayList<>();
-		done = false;
-	}
+    /**
+     * Método principal que crea una instancia del servidor y ejecuta el método run.
+     *
+     * @param args Argumentos de la línea de comandos (no se utilizan en este caso).
+     */
+    public static void main(String[] args) {
+    	Server server = new Server();
+    	server.run();
+    }
+    
+    /**
+     * Constructor de la clase Server.
+     */
+    public Server() {
+        connections = new ArrayList<>();
+        done = false;
+    }
 
-	@Override
-	public void run() {
-	    try {
-	    	int port = 8080;
-	        server = new ServerSocket(port);
-	        pool = Executors.newCachedThreadPool();
-			System.out.println("Servidor iniciado en el puerto: " + port);
-	        while (!done) {
-	            Socket client = server.accept();
-	            ConnectionHandler handler = new ConnectionHandler(client);
-	            connections.add(handler);
-	            pool.execute(handler);
-	        }
-	    } catch (Exception e) {
-	        shutdown(); 
-	    }
-	}
+    /**
+     * El método run se ejecuta cuando se inicia un hilo para este servidor.
+     */
+    @Override
+    public void run() {
+        try {
+            int port = 8080;
+            server = new ServerSocket(port);
+            pool = Executors.newCachedThreadPool();
+            System.out.println("Servidor iniciado en el puerto: " + port);
+            while (!done) {
+                Socket client = server.accept();
+                ConnectionHandler handler = new ConnectionHandler(client);
+                connections.add(handler);
+                pool.execute(handler);
+            }
+        } catch (Exception e) {
+            shutdown();
+        }
+    }
 
-	public void broadcast(String message, String nickname) {
-		for (ConnectionHandler ch : connections) {
-			if (ch != null && ch.nickname != nickname) {
-				ch.sendMessage(message);
-			}
-		}
-	}
+    /**
+     * Este método se utiliza para enviar un mensaje a todos los clientes conectados, excepto al que lo envió.
+     *
+     * @param message   El mensaje a enviar.
+     * @param nickname  El apodo del remitente del mensaje.
+     */
+    public void broadcast(String message, String nickname) {
+        for (ConnectionHandler ch : connections) {
+            if (ch != null && !ch.nickname.equals(nickname)) {
+                ch.sendMessage(message);
+            }
+        }
+    }
 
-	public void shutdown() {
-		try {
-			done = true;
-			pool.shutdown();
-			if (!server.isClosed()) {
-				server.close();
-			}
-			for (ConnectionHandler ch : connections) {
-				ch.shutdown();
-			}
-		} catch (IOException e) {
-			// ignore
-		}
+    /**
+     * Este método se utiliza para cerrar todas las conexiones y limpiar recursos.
+     */
+    public void shutdown() {
+        try {
+            done = true;
+            pool.shutdown();
+            if (!server.isClosed()) {
+                server.close();
+            }
+            for (ConnectionHandler ch : connections) {
+                ch.shutdown();
+            }
+        } catch (IOException e) {
+            // ignore
+        }
+    }
 
-	}
+    /**
+     * Clase interna que representa el manejador de conexión para un cliente.
+     */
+    class ConnectionHandler implements Runnable {
 
-	class ConnectionHandler implements Runnable {
+        private Socket client;
+        private BufferedReader in;
+        private PrintWriter out;
+        private String nickname;
 
-		private Socket client;
-		private BufferedReader in;
-		private PrintWriter out;
-		private String nickname;
+        /**
+         * Constructor de la clase ConnectionHandler.
+         *
+         * @param client El socket de cliente asociado a este manejador de conexión.
+         */
+        public ConnectionHandler(Socket client) {
+            this.client = client;
+        }
 
-		public ConnectionHandler(Socket client) {
-			this.client = client;
-		}
+        @Override
+        public void run() {
+            try {
+                out = new PrintWriter(client.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                out.println("Ingrese un nombre de usuario: ");
+                nickname = in.readLine();
+                System.out.println("Usuario " + nickname + " conectado");
+                broadcast(nickname + " se ha unido al chat!", null);
+                String message;
+                while ((message = in.readLine()) != null) {
+                    if (message.equalsIgnoreCase("chao")) {
+                        System.out.println("Usuario " + nickname + " desconectado");
+                        broadcast("El usuario " + nickname + " abandonó", null);
+                        shutdown();
+                    } else {
+                        System.out.println(nickname + ": " + message);
+                        broadcast(nickname + ": " + message, nickname);
+                    }
+                }
+            } catch (IOException e) {
+                shutdown();
+            }
+        }
 
-		@Override
-		public void run() {
-			try {
-				out = new PrintWriter(client.getOutputStream(), true);
-				in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-				out.println("Ingrese un nombre de usuario: ");
-				nickname = in.readLine();
-				System.out.println("Usuario " + nickname + " conectado");
-				broadcast(nickname + " se ha unido al chat!", null);
-				String message;
-				while ((message = in.readLine()) != null) {
-					if (message.equalsIgnoreCase("chao")) {
-						System.out.println("Usuario " + nickname + " desconectado");
-						broadcast("El usuario " + nickname + " abandonó", null);
-						shutdown();
-					} else {
-						System.out.println(nickname + ": " + message);
-						broadcast(nickname + ": " + message, nickname);
-					}
-				}
-			} catch (IOException e) {
-				shutdown();
-			}
-		}
+        /**
+         * Este método se utiliza para enviar un mensaje al cliente asociado a este manejador de conexión.
+         *
+         * @param message El mensaje a enviar.
+         */
+        public void sendMessage(String message) {
+            out.println(message);
+        }
 
-		public void sendMessage(String message) {
-			out.println(message);
-		}
+        /**
+         * Este método se utiliza para cerrar la conexión y limpiar recursos.
+         */
+        public void shutdown() {
+            try {
+                in.close();
+                out.close();
+                if (!client.isClosed()) {
+                    client.close();
+                }
+            } catch (IOException e) {
+                // ignore
+            }
+        }
+    }
 
-		public void shutdown() {
-			try {
-				in.close();
-				out.close();
-				if (!client.isClosed()) {
-					client.close();
-				}
-			} catch (IOException e) {
-				// ignore
-			}
-		}
-	}
-
-	public static void main(String[] args) {
-		Server server = new Server();
-		server.run();
-	}
 }
